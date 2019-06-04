84732237453d307365b0ce945ad60739f9c26b1c
RasheedElKassed <relkassed@gmail.com>
2019-06-04 02:29:32 -0700

    Add lazy implementation of string summations

--- Java/src/SummationExecutor.java
+++ Java/src/SummationExecutor.java
@@ -1,3 +1,7 @@
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
 public class SummationExecutor {
     public SummationExecutor()
     { }
@@ -8,10 +12,28 @@ public class SummationExecutor {
         System.out.println("Result of summation: " + result);
     }
 
+    public static void executeStringSummation(String form, float i, float n) throws ScriptException
+    {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("js");
+        Double result = 0.0;
+        for(; i <= n; i++) {
+            engine.put("n", i);
+            result += (Double)engine.eval(form);
+        }
+        System.out.println("Result of summation: " + result);
+    }
+
     public static void main(String[] args) {
         System.out.println("Summation formula 1 from 1 to 5:");
         executeSummation(new SummationFormula1(), 1,5);
         System.out.println("Summation formula 2 from 1 to 5:");
         executeSummation(new SummationFormula2(),1,5);
+        System.out.println("Summation formula string from 1 to 5:");
+        try {
+            executeStringSummation("(n * (n + 1) * ((2 * n) + 1))/6", 1, 5);
+        } catch (ScriptException e) {
+            System.out.println(e);
+        }
     }
 }
